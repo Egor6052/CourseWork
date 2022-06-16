66463f62d98a756212764540486be836c4d2fe7d
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-06-16 12:26:08 +0300

    Update index.js

--- Web курсовая/src/js/index.js
+++ Web курсовая/src/js/index.js
@@ -16,7 +16,7 @@ function templateGenerator(list) {
     $list.innerHTML = template;
 }
 
-fetch('http://localhost:8001/back?name=JSON&count=2')
+fetch('http://localhost:8001/back')
     .then(function(responce) {
         return responce.json();
     })
